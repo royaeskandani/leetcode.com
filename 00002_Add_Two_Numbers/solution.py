666e689e4c2da35c619db4a30f863d49f70d4980
Roya Elisa Eskandani <roya.eskandani@gmail.com>
2023-05-14 17:40:30 +0200

    Solved 2. Add Two Numbers

--- 00002_Add_Two_Numbers/solution.py
+++ 00002_Add_Two_Numbers/solution.py
@@ -3,5 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+  def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    out = ListNode()
+    current = out
+    overflow = 0
+
+    while l1 or l2 or overflow:
+      value = overflow
+      if l1:
+          value += l1.val
+          l1 = l1.next
+      if l2:
+          value += l2.val
+          l2 = l2.next
+
+      overflow, value = divmod(value, 10)
+      current.next = ListNode(value)
+      current = current.next
+
+    return out.next
