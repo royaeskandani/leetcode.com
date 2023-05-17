e99d3f5db47a4a7d02c963cda0707733482d50ee
Roya Eskandani <roya@eskandani.de>
2023-05-17 12:57:17 +0200

    Solved 242 Valid Anagram

---
+++ 00242_Valid_Anagram/solution.py
@@ -0,0 +1,25 @@
+import enum
+from typing import Optional, List
+
+
+class Solution:
+  def __init__(self) -> None:
+    pass
+    
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+    
+    for e in set(s):
+      if s.count(e) != t.count(e):
+        return False
+    return True
+                
+        
+        
+if __name__ == "__main__":
+  test = Solution()
+  
+  assert(test.isAnagram(s = "anagram", t = "nagaram") == True)
+  assert(test.isAnagram(s = "rat", t = "car") == False)
---
+++ 00242_Valid_Anagram/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+n range(len(s)):
+    #   print
